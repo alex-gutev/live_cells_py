9e9e72310aa37145fc01644f74b71d15e5e99685
Alexander Gutev <alex.gutev@gmail.com>
2024-04-25 20:16:50 +0200

    Add unit tests for changes_only option.

--- tests/test_watch.py
+++ tests/test_watch.py
@@ -1,4 +1,4 @@
-from live_cells import watch, mutable, batch
+from live_cells import watch, mutable, batch, computed
 from .util import LifecycleCounter, LifecycleTestCell
 
 class WatchTracker:
@@ -158,3 +158,162 @@ class TestCellWatcher:
 
         assert counter.count_init == 1
         assert counter.count_dispose == 0
+
+class TestChangesOnly:
+    """Test the changesOnly option"""
+
+    def test_watch_not_called_when_value_unchanged(self, test_watch):
+        """Test that the watch function is not called when the value has not changed."""
+
+        a = mutable([1, 2, 3])
+        b = computed(lambda: a()[1], changes_only=True)
+
+        track = WatchTracker()
+
+        @test_watch
+        def watch_b():
+            track.add(b())
+
+        a.value = [4, 2, 6]
+
+        assert track.values == [2]
+
+    def test_watch_not_called_when_value_unchanged_in_batch(self, test_watch):
+        """Test that the watch function is not called when the value has not changed with batch()."""
+
+        a = mutable([1, 2, 3])
+        b = computed(lambda: a()[1], changes_only=True)
+
+        track = WatchTracker()
+
+        @test_watch
+        def watch_b():
+            track.add(b())
+
+        with batch():
+            a.value = [4, 2, 6]
+
+        assert track.values == [2]
+
+    def test_watch_called_when_value_changed_after_not_changing(self, test_watch):
+        """Test that the watch function is called when the value has changed.
+
+        This tests value changes that happen after a value change
+        which did not result in the value of the cell actually
+        changing.
+
+        """
+
+        a = mutable([1, 2, 3])
+        b = computed(lambda: a()[1], changes_only=True)
+
+        track = WatchTracker()
+
+        @test_watch
+        def watch_b():
+            track.add(b())
+
+        a.value = [4, 2, 6]
+        a.value = [7, 8, 9]
+
+        assert track.values == [2, 8]
+
+    def test_watch_called_when_one_argument_changes(self, test_watch):
+        """Test that the watch function is called when at least one argument changes."""
+
+        a = mutable([1, 2, 3])
+        b = computed(lambda: a()[1], changes_only=True)
+
+        c = mutable(3)
+
+        track = WatchTracker()
+
+        @test_watch
+        def watch_b_c():
+            track.add((b(), c()))
+
+        with batch():
+            a.value = [4, 2, 6]
+            c.value = 5
+
+        assert track.values == [(2, 3), (2, 5)]
+
+    def test_computed_cell_not_recomputed_when_arguments_not_changed(self, test_watch):
+        """Test that the value of a computed cell is not recomputed when none of the arguments change."""
+
+        a = mutable([1, 2, 3])
+        b = computed(lambda: a()[1], changes_only=True)
+        c = computed(lambda: b() * 10)
+
+        track = WatchTracker()
+
+        @test_watch
+        def watch_c():
+            track.add(c())
+
+        a.value = [4, 2, 6]
+
+        assert track.values == [20]
+
+    def test_computed_cell_not_recomputed_when_arguments_not_changed_in_batch(self, test_watch):
+        """Test that the value of a computed cell is not recomputed when none of the arguments change in batch()."""
+
+        a = mutable([1, 2, 3])
+        b = computed(lambda: a()[1], changes_only=True)
+        c = computed(lambda: b() * 10)
+
+        track = WatchTracker()
+
+        @test_watch
+        def watch_c():
+            track.add(c())
+
+        with batch():
+            a.value = [4, 2, 6]
+
+        assert track.values == [20]
+
+    def test_computed_cell_recomputed_when_arguments_change(self, test_watch):
+        """Test that the value of a computed cell is recomputed when the arguments change.
+
+        This tests that the computed cell is recomputed when the
+        values of the arguments change AFTER they having not changed
+        during the previous update cycle.
+
+        """
+
+        a = mutable([1, 2, 3])
+        b = computed(lambda: a()[1], changes_only=True)
+        c = computed(lambda: b() * 10)
+
+        track = WatchTracker()
+
+        @test_watch
+        def watch_c():
+            track.add(c())
+
+        a.value = [4, 2, 6]
+        a.value = [7, 8, 9]
+
+        assert track.values == [20, 80]
+
+    def test_computed_cell_recomputed_when_one_argument_changes(self, test_watch):
+        """Test that the value of a computed cell is recomputed when the only one argument changes."""
+
+        a = mutable([1, 2, 3])
+        b = computed(lambda: a()[1], changes_only=True)
+
+        c = mutable(3)
+        d = computed(lambda: b() * c())
+
+        track = WatchTracker()
+
+        @test_watch
+        def watch_d():
+            track.add(d())
+
+        with batch():
+            a.value = [4, 2, 6]
+            c.value = 5
+
+        assert track.values == [6, 10]
