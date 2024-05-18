094145a65e519283e1b34b4b881a668a33b019a2
Alexander Gutev <alex.gutev@gmail.com>
2024-05-18 09:01:44 +0200

    Improve docstring of computed() function.

--- live_cells/computed.py
+++ live_cells/computed.py
@@ -4,21 +4,32 @@ from .exceptions import StopComputeException
 def computed(compute=None, key = None, changes_only = False):
     """Create a computed cell with dynamically determined arguments.
 
-    A computed cell with compute function `compute` is
-    created. `compute` is a function of no arguments, that is called
+    A computed cell with compute function ``compute`` is
+    created. ``compute`` is a function of no arguments, that is called
     to compute the value of the cell. The cells referenced within
-    `compute`, using the function call syntax, are automatically
-    registered as dependencies of the cell.
+    ``compute``, using the function call syntax, are automatically
+    registered as dependencies of the cell such that when their values
+    change the value of the computed cell is recomputed.
 
-    The cell is identified by `key` if it is not None.
+    .. note::
 
-    If `changes_only` is True, the cell only notifies its observers if
-    its value has actually changed.
+       This function may be used as a decorator by omitting the
+       ``compute`` argument, in which case the decorated function is
+       used as the compute function. The cell is then referenced using
+       the name of the decorated function.
 
-    This function may be used as a decorator by omitting the `compute`
-    argument, in which case the decorated function is used as the
-    compute function. The cell can then be referenced using the name
-    of the decorated function.
+    :param compute: Function of no arguments called to compute the\
+    value of the cell.
+
+    :type compute: function
+
+    :param key: Key identifying the cell if not *None*. Defaults to *None*.
+
+    :param changes_only: If *True* the cell only notifies its\
+    observers if its new value is not equal to its previous\
+    value. Defaults to *False*.
+
+    :returns: A computed cell.
 
     """
 
