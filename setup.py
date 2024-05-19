b6d86577e943f0b341434cc42779a0bfce3c0a40
Alexander Gutev <alex.gutev@gmail.com>
2024-05-19 20:48:27 +0200

    Change package name in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='Live Cells Python',
+    name='live-cells-py',
     version='0.1.0',
     author='Alexander Gutev',
     author_email='alex.gutev@gmail.com',
