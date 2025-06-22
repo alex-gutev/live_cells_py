3b27ccb6d4ef1c7f1a9de4631bebad22178c9f9a
Alexander Gutev <alex.gutev@gmail.com>
2025-06-22 12:36:20 +0200

    Update release in docs/conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'Live Cells Python'
 copyright = '2024, Alexander Gutev'
 author = 'Alexander Gutev'
-release = '0.1'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
